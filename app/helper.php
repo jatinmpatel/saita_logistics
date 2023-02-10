527f3e15e2797dc6504ea24bc39039da60b00614
unknown <pateljatin.m7893@gmail.com>
2023-02-10 20:31:16 +0200

    changes in helper show role wise module

--- app/helper.php
+++ app/helper.php
@@ -4,7 +4,7 @@ if(!function_exists('mainMenu')) {
     function mainMenu(){
         $permission = \DB::table('user_permission')->select('module_route')->where('view_permission',0)->where('user_id',Auth::user()->id)->get();
         if(count($permission)==0){
-            $permission = \DB::table('role_permission')->select('module_route')->where('role_id',Auth::user()->intRole)->get();
+            $permission = \DB::table('role_permission')->select('module_route')->where('role_id',Auth::user()->role_id)->where('view_permission',0)->get();
         }
         return $permission->pluck('module_route')->toArray();
     }
@@ -15,7 +15,7 @@ if(!function_exists('checkAccess')) {
         $access = false;
         $permission = \DB::table('user_permission')->select('id')->where('module_route',$module)->where('user_id',Auth::user()->id)->first();
         if(!isset($permission->id)){
-            $permission = \DB::table('role_permission')->select('id')->where('module_route',$module)->where($accessPermission,0)->where('role_id',Auth::user()->intRole)->first();
+            $permission = \DB::table('role_permission')->select('id')->where('module_route',$module)->where($accessPermission,0)->where('role_id',Auth::user()->role_id)->first();
             if(isset($permission->id)){
                 $access = true;
             }
